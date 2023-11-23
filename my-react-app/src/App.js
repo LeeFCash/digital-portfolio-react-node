bf2facbbab3768b146ff97aa222110c9a8ffb64c
LeeFCash <leecash133@gmail.com>
2023-11-22 20:45:54 -0500

    toggle text

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -3,17 +3,25 @@ import './App.css';
 
 function App() {
   const [data, setData] = useState('');
+  const testName = 'check the server port 5000';
+  var testForNull = null;
 
 useEffect(() => {
   fetch('http://localhost:5000')
   .then(response => response.text())
   .then(data => setData(data))
   .catch(error => console.error('error:', error));
-}, []);
+});
 
   return (
     <div className="text">
-      <h1>{data}</h1>
+      {testForNull ? (
+        <>
+          test
+        </>
+      ) : (
+        <h1>{ null ? testName : data }</h1>
+      )}
     </div>
   );
 }
