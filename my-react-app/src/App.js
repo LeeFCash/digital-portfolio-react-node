833cf65a4e98ea36386d917c81eb974d04c3c51e
Lee Cash <118697195+LeeFCash@users.noreply.github.com>
2023-12-19 22:10:34 +0000

    tabs switching is done but not the content inside

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 
 function App() {
   const [data, setData] = useState('');
+  const [tab, setTab] = useState(0);
   const testName = 'check the server port 5000';
   var mainAllOverAllContent = document.getElementById('mainAll-overAllContent');
   var startPage1 = document.getElementById('mainAll-overAllContent-content1');
@@ -15,48 +16,13 @@ function App() {
   var tab2p2 = false;
   var tab3p2 = false;
   function tab1() {
-    /*if(tab1p2 === false && tab2p2 === false && tab3p2 === false && startPage1p2 === true) {
-      startPage1.remove();
-      workSamples.remove();
-      contactMe.remove();
-      mainAllOverAllContent.appendChild(aboutMeContent);
-      tab1p2 = true;
-      startPage1p2 = false;
-      } else if(tab1p2 === false && tab2p2 === false && tab3p2 === true && startPage1p2 === false){
-          startPage1.remove();
-          workSamples.remove();
-          contactMe.remove();
-          mainAllOverAllContent.appendChild(aboutMeContent);
-          tab1p2 = true;
-          tab3p2 = false
-      } else if(tab1p2 === false && tab2p2 === true && tab3p2 === false && startPage1p2 === false) {
-          startPage1.remove();
-          workSamples.remove();
-          contactMe.remove();
-          mainAllOverAllContent.appendChild(aboutMeContent);
-          tab1p2 = true;
-          tab2p2 = false
-      } else if(tab1p2 === true && tab2p2 === false && tab3p2 === false && startPage1p2 === false) {
-          alert('This tab is already open.');
-      } else {
-          alert('Something unexpected happen.');
-      };*/
-
-      if(tab1p2 === false && tab2p2 === false && tab3p2 === false && startPage1p2 === true) {
-        tab1p2 = true;
-        startPage1p2 = false;
-        } else if(tab1p2 === false && tab2p2 === false && tab3p2 === true && startPage1p2 === false){
-            tab1p2 = true;
-            tab3p2 = false
-        } else if(tab1p2 === false && tab2p2 === true && tab3p2 === false && startPage1p2 === false) {
-            tab1p2 = true;
-            tab2p2 = false
-        } else if(tab1p2 === true && tab2p2 === false && tab3p2 === false && startPage1p2 === false) {
-            alert('This tab is already open.');
-        } else {
-            alert('Something unexpected happen.');
-        };
-      return tab1p2, tab1p2, tab1p2, startPage1p2;
+    setTab(1);
+  };
+  function tab2() {
+    setTab(2);
+  };
+  function tab3() {
+    setTab(3);
   };
 
   function initialLoad() {
@@ -78,45 +44,25 @@ function App() {
       <div className="mainAll-tabBarDiv">
        <ol className="mainAll-tabBarDiv-orderList">
         <button onClick={tab1} className="mainAll-tabBarDiv-orderList-tab1">About Me</button>
-        <button className="mainAll-tabBarDiv-orderList-tab2">My work sample</button>
-        <button className="mainAll-tabBarDiv-orderList-tab3">Contact me</button>
+        <button onClick={tab2} className="mainAll-tabBarDiv-orderList-tab2">My work sample</button>
+        <button onClick={tab3} className="mainAll-tabBarDiv-orderList-tab3">Contact me</button>
        </ol>
       </div>
       <div className="mainAll-overAllContent">
-      {startPage1p2 ? (
-      <><div className="mainAll-content1" id="mainAll-overAllContent-content1">
+       <div className="mainAll-content1" id="mainAll-overAllContent-content1" hidden={tab != 0}>
         <h1 className="mainAll-content1-h11">Hello, I'm Lee Cash you can get to know me better by clicking about me, my work sample or contact me.</h1>
        </div>
-       </>
-       ): (
-        <></>
-       )}
-       {tab1p2 ? (
-       <><div className="mainAll-overAllContent-content2" id="mainAll-overAllContent-content2" >
+       <div className="mainAll-overAllContent-content2" id="mainAll-overAllContent-content2" hidden={tab != 1}>
          <p className="mainAll-overAllContent-content2-name">Name:</p>
          <p className="mainAll-overAllContent-content2-age">age:</p>
          <p className="mainAll-overAllContent-content2-education">education:</p>
        </div>
-       </>
-       ): (
-        <></>
-       )}
-       {tab2p2 ? (
-       <><div className="mainAll-overAllContent-content3" id="mainAll-overAllContent-content3">
+       <div className="mainAll-overAllContent-content3" id="mainAll-overAllContent-content3" hidden={tab != 2}>
          <p>tab2</p>
        </div>
-       </>
-       ): (
-        <></>
-       )}
-       {tab3p2 ? (
-       <><div className="mainAll-overAllContent-content4" id="mainAll-overAllContent-content3">
+       <div className="mainAll-overAllContent-content4" id="mainAll-overAllContent-content3" hidden={tab != 3}>
          <p>tab3</p>
        </div>
-       </>
-       ): (
-        <></>
-       )}
       </div>
      </div>
     </div>
